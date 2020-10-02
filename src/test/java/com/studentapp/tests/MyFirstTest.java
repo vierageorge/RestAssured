ca976e85c64bc7721ef8b3297070cd16a784556a
vierageorge <joviera@gmail.com>
2020-10-02 09:06:46 -0500

    Statically import RestAssured methods

--- src/test/java/com/studentapp/tests/MyFirstTest.java
+++ src/test/java/com/studentapp/tests/MyFirstTest.java
@@ -3,20 +3,19 @@ package com.studentapp.tests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import io.restassured.RestAssured;
-
+import static io.restassured.RestAssured.*;
 
 public class MyFirstTest {
-	
+
 	@DisplayName("Getting all the students from the database")
 	@Test
 	void getAllStudents() {
-		
-		RestAssured.given()
+
+		given()
 			.expect()
 			.statusCode(201)
 			.when()
 			.get("http://localhost:8085/student/list");
-		
+
 	}
 }
