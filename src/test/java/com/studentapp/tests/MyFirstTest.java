d80caed42b4577206e85de59e9f6a72b25d9d319
vierageorge <joviera@gmail.com>
2020-10-02 09:27:08 -0500

    One way of passing queryparams

--- src/test/java/com/studentapp/tests/MyFirstTest.java
+++ src/test/java/com/studentapp/tests/MyFirstTest.java
@@ -3,6 +3,8 @@ package com.studentapp.tests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.*;
 
 public class MyFirstTest {
@@ -17,5 +19,19 @@ public class MyFirstTest {
 			.when()
 			.get("http://localhost:8085/student/list");
 
+	}
+	
+	@DisplayName("Get a CS Student from the database")
+	@Test
+	void getSingleCSStudent() {
+		Response response =
+		given()
+			.queryParam("programme", "Computer Science")
+			.queryParam("limit", 1)
+			.when()
+			.get("http://localhost:8085/student/list");
+		
+		response.prettyPrint();
+			
 	}
 }
