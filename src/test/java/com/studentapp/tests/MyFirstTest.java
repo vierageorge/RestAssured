f21e384def3458f8aeeb0378d6afd02ff684a09c
vierageorge <joviera@gmail.com>
2020-10-02 08:50:33 -0500

    Refactor Tests to more readable format

--- src/test/java/com/studentapp/tests/MyFirstTest.java
+++ src/test/java/com/studentapp/tests/MyFirstTest.java
@@ -4,20 +4,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
-import io.restassured.specification.RequestSpecification;
+
 
 public class MyFirstTest {
 	
 	@DisplayName("Getting all the students from the database")
 	@Test
 	void getAllStudents() {
-		RequestSpecification requestSpec = RestAssured.given();
-		Response response = requestSpec.get("http://localhost:8085/student/list");
-		response.prettyPrint();
-		ValidatableResponse validatableResponse = response.then();
-		validatableResponse.statusCode(200);
+		
+		RestAssured.given()
+			.when()
+			.get("http://localhost:8085/student/list")
+			.then()
+			.statusCode(200);
 		
 	}
 }
